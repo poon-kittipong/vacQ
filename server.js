339c865c8a9af859ca9ac17a12cd46d928acd27b
Kittipong <kittipong.deevee@gmail.com>
2022-02-01 14:03:53 +0700

    Create route for hospitals.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Route files.
+const hospitals = require("./routes/hospitals");
+
+// Load env vars.
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+// Mount hospitals
+app.use("/api/v1/hospitals", hospitals);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    "Server is running in",
+    process.env.NODE_ENV,
+    "mode on port",
+    PORT
+  )
+);
