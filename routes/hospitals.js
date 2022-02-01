5b123f0d8214a6387ce6c041c72b1b8cfcd364ec
Kittipong <kittipong.deevee@gmail.com>
2022-02-01 14:32:40 +0700

    Create controller file.

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,34 +1,20 @@
 const express = require("express");
+const {
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
+} = require("../controllers/hospitals");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } });
-});
+router.route("/").get(getHospitals).post(createHospital);
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all hospitals" });
-});
-
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show hospital : ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: `Create new hospitals` });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update hospital : ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete hospital : ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .get(getHospital)
+  .put(updateHospital)
+  .delete(deleteHospital);
 
 module.exports = router;
