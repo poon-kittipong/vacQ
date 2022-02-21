9df4df804f7b1e08cb07eb1893a7a4f3d491338d
Kittipong <kittipong.deevee@gmail.com>
2022-02-21 15:47:55 +0700

    Add Authentication

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 // Route files.
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
 
 // Load env vars.
 dotenv.config({ path: "./config/config.env" });
@@ -17,8 +18,12 @@ const app = express();
 // Body parser
 app.use(express.json());
 
+// Cookie parser
+app.use(cookieParser());
+
 // Mount hospitals
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
